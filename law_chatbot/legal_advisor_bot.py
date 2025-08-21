633e4e6421c85367e4d561adcb3bf4416eb87f11
Adnan <adnan.0707004@gmail.com>
2025-08-21 07:49:26 -0700

    Update legal_advisor_bot.py

--- law_chatbot/legal_advisor_bot.py
+++ law_chatbot/legal_advisor_bot.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import RetrievalQA
